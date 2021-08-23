b790206fe711d5181dafd57f2891b455ed1b5b9e
John Novak <jnovak@skedulo.com>
2021-08-23 17:58:30 +1000

    hocon: unicode scanner working

--- src/hocon.nim
+++ src/hocon.nim
@@ -2,6 +2,7 @@ import deques
 import unicode
 import streams
 import strformat
+import strutils
 
 # {{{ UnicodeScanner
 
@@ -12,40 +13,33 @@ type
     peekBuf: Deque[Rune]
     line, column: Natural
 
-
 using s: var UnicodeScanner
 
-proc initUnicodeScanner(stream: Stream): UnicodeScanner =
+proc initUnicodeScanner*(stream: Stream): UnicodeScanner =
   result.stream = stream
   result.readBuf = newStringOfCap(4)
   result.readBuf.setLen(4)
   result.peekBuf = initDeque[Rune]()
 
-proc readRune(s): Rune =
-#  if s.peekBuf.len > 0:
-#    s.peekBuf.popFirst()
-#  else:
-    s.readBuf[0] = cast[char](s.stream.readUint8())
-    echo fmt"buf: {cast[uint8](s.readBuf[0]):02x}"
+proc doReadRune(s): Rune =
+  s.readBuf[0] = cast[char](s.stream.readUint8())
+  let runeLen = s.readBuf.runeLenAt(0)
+  let bytesRead = s.stream.readDataStr(s.readBuf, 1..(1 + runeLen-2))
+  if bytesRead + 1 != runeLen:
+    raise newException(IOError, "Unexpected end of file")
+  s.readBuf.runeAt(0)
 
-    let len = s.readBuf.runeLenAt(0)
-    echo "len ", len
-    let bytesRead = s.stream.readDataStr(s.readBuf, 1..(1 + len-1))
-    echo fmt"buf: {cast[uint8](s.readBuf[0]):02x} {cast[uint8](s.readBuf[1]):02x} {cast[uint8](s.readBuf[2]):02x} {cast[uint8](s.readBuf[3]):02x}"
-    echo ""
-  #  if bytesRead != len:
-  #    raiseError()
-    let r = s.readBuf.runeAt(0)
-    echo r
-    r
+proc readRune*(s): Rune =
+  if s.peekBuf.len > 0: s.peekBuf.popFirst()
+  else: doReadRune(s)
 
-proc peekRune(s; lookahead: Natural = 1): Rune =
+proc peekRune*(s; lookahead: Natural = 1): Rune =
   assert(lookahead >= 1)
   while lookahead > s.peekBuf.len:
-    s.peekBuf.addLast(readRune(s))
+    s.peekBuf.addLast(doReadRune(s))
   s.peekBuf[lookahead-1]
 
-proc close(s) =
+proc close*(s) =
   s.stream.close()
   s.stream = nil
 
@@ -77,29 +71,63 @@ type
 # }}}
 
 when isMainModule:
-  let a = initDeque[Rune]()
-  let testString = "\u0024\u00a2\u0939\u20ac\ud5cc\u10348"
-  # byteLen            1     2     3     3     3     4
-  # byteOffs           0     1     3     6     9    12
-  
-#[
-  echo testString.runeLenAt(0)
-  echo testString.runeLenAt(1)
-  echo testString.runeLenAt(3)
-  echo testString.runeLenAt(6)
-  echo testString.runeLenAt(9)
-  echo testString.runeLenAt(12)
+  let testString = "\u0024\u00a2\u0939\u20ac\ud5cc"
+  # byteLen            1     2     3     3     3
+  # byteOffs           0     1     3     6     9
 
-  echo "\u1034\u0008".runeLenAt(0)
-]#
+  let
+    rune1 = Rune(0x0024)
+    rune2 = Rune(0x00a2)
+    rune3 = Rune(0x0939)
+    rune4 = Rune(0x20ac)
+    rune5 = Rune(0xd5cc)
 
-  var s = initUnicodeScanner(newStringStream(testString))
-  assert s.readRune() == Rune(0x0024)
-  assert s.readRune() == Rune(0x00a2)
+  block: # read test
+    var s = initUnicodeScanner(newStringStream(testString))
+    assert s.readRune() == rune1
+    assert s.readRune() == rune2
+    assert s.readRune() == rune3
+    assert s.readRune() == rune4
+    assert s.readRune() == rune5
 
-  assert s.readRune() == Rune(0x0939)
-  assert s.readRune() == Rune(0x20ac)
-  assert s.readRune() == Rune(0xd5cc)
-  assert s.readRune() == Rune(0x10348)
+    try:
+      discard s.readRune()
+      assert false
+    except IOError: discard
+
+  block: # peek test
+    var s = initUnicodeScanner(newStringStream(testString))
+    assert s.peekRune()  == rune1
+    assert s.peekRune()  == rune1
+    assert s.readRune()  == rune1
+
+    assert s.peekRune()  == rune2
+    assert s.peekRune(2) == rune3
+    assert s.peekRune()  == rune2
+    assert s.peekRune(2) == rune3
+    assert s.peekRune(2) == rune3
+    assert s.peekRune(3) == rune4
+    assert s.peekRune(4) == rune5
+    assert s.peekRune(4) == rune5
+    assert s.peekRune()  == rune2
+    assert s.peekRune(2) == rune3
+    assert s.peekRune(3) == rune4
+    assert s.peekRune(4) == rune5
+
+    try:
+      discard s.peekRune(5)
+      assert false
+    except IOError: discard
+
+    assert s.readRune()  == rune2
+    assert s.readRune()  == rune3
+    assert s.readRune()  == rune4
+    assert s.readRune()  == rune5
+
+    try:
+      discard s.peekRune()
+      assert false
+    except IOError: discard
+
 
 # vim: et:ts=2:sw=2:fdm=marker
