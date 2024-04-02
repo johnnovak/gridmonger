9dfdb3f70ef4289daac7723c4a4163ed005463cf
John Novak <john@johnnovak.net>
2024-04-02 16:16:10 +1000

    Fix naturalSortIgnoreCaseUtf8

--- src/naturalsort.nim
+++ src/naturalsort.nim
@@ -3,37 +3,12 @@ import std/parseutils
 import std/strutils
 import std/unicode
 
-# This is a slight adaptation of Alogani's code from here:
+# This is an adaptation of Alogani's code from here:
 # https://github.com/nim-lang/Nim/issues/23462
 
-# {{{ isDigit*()
-# List of zero converted from ttps://www.fileformat.info/info/unicode/category/Nd/list.htm
-const allZeros = [
-  48,    1632,  1776,  1984,  2406, 2534, 2662, 2790,  2918,  3046,  3174,
-  3302,  3430,  3558,  3664,  3792, 3872, 4160, 4240,  6112,  6160,  6470,
-  6608,  6784,  6800,  6992,  7088, 7232, 7248, 42528, 43216, 43264, 43472,
-  43504, 43600, 44016, 65296, 4170, 4307, 4358, 4367,  4371,  4381,  4399,
-  4421,  4429,  4453,  4460,  4467, 4494, 4501, 4549,  4565,  4570,  4597,
-  5798,  5804,  5813,  7548,  7549, 7550, 7550, 7551,  7700,  7727,  7759,
-  7829,  8127
-]
-
-func toDigitImpl(r: Rune): int =
-  let codePoint = ord(r)
-  for z in allZeros:
-    # not a binary search, because first runes are more common
-    if codePoint > z:
-      return codePoint - z
-  return -1
-
-func isDigit*(r: Rune): bool =
-  let digit = r.toDigitImpl
-  digit in {0..9}
-
-func toDigit*(r: Rune): int =
-  result = r.toDigitImpl
-  if result notin {0..9}:
-    raise newException(RangeDefect, "rune is not a valid digit")
+# {{{ isAsciiDigit()
+proc isAsciiDigit(r: Rune): bool =
+  ord(r) >= ord('0') and ord(r) <= ord('9')
 
 # }}}
 # {{{ rawParseInt()
@@ -53,7 +28,8 @@ proc rawParseInt(s: openArray[Rune], b: var BiggestInt): int =
 
   if i < s.len:
     b = 0
-    while i < s.len and (let c = toDigitImpl(s[i]); c in {0..9}):
+    while i < s.len and s[i].isAsciiDigit:
+      let c = ord(s[i])
       if b >= (low(BiggestInt) + c) div 10:
         b = b * 10 - c
       else:
@@ -88,7 +64,8 @@ template cmpNaturalImpl(a, b: string, comparator: untyped): auto =
       let diff = comparator(a[ai], b[bi])
       if diff != 0:
         return diff
-      ai += 1; bi += 1
+      inc(ai)
+      inc(bi)
     else:
       var
         aNum: int
@@ -105,11 +82,12 @@ template cmpNaturalImpl(a, b: seq[Rune], comparator: untyped): auto =
   while true:
     if ai > high(a) or bi > high(b):
       return a.len - ai - b.len + bi
-    if not(a[ai].isDigit and b[bi].isDigit):
+    if not(a[ai].isAsciiDigit and b[bi].isAsciiDigit):
       let diff = comparator(a[ai], b[bi])
       if diff != 0:
         return diff
-      ai += 1; bi += 1
+      inc(ai)
+      inc(bi)
     else:
       var
         aNum: Biggestint
@@ -167,7 +145,7 @@ func naturalSortIgnoreCase*(l: openArray[string]): seq[string] =
 func naturalSortIgnoreCase*(l: openArray[seq[Rune]]): seq[seq[Rune]] =
   l.sorted(cmpNaturalIgnoreCase)
 
-proc naturalSortIgnoreCase*(l: openArray[string]): seq[string] =
+proc naturalSortIgnoreCaseUtf8*(l: openArray[string]): seq[string] =
   var rl = newSeq[seq[Rune]](l.len)
   for i in 0..<l.len:
     rl[i] = l[i].toRunes
@@ -187,7 +165,7 @@ when isMainModule:
             "cdrom101", "cdrom103", "cdrom110"]
 
 
-  echo a.naturalSort
+  echo a.naturalSortIgnoreCaseUtf8
 
   var b = @["!a", "[b"]
 
