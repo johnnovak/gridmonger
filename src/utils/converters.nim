e02ccda59da345ef628416b5045dcb5311f0dbe3
John Novak <john@johnnovak.net>
2024-07-02 17:47:27 +1000

    Extend converters

--- src/utils/converters.nim
+++ src/utils/converters.nim
@@ -1,2 +1,5 @@
 converter toCfloat*(x: SomeInteger): cfloat = x.cfloat
 converter tcFloat*(x: SomeInteger): float = x.float
+
+converter toInt32Tuple*(t: (Natural, Natural)): (int32, int32) =
+  (t[0].int32, t[1].int32)
