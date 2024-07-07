f39f26c3bd73f15d674f6236971ebc113067251c
John Novak <john@johnnovak.net>
2024-07-07 12:00:30 +1000

    Improve macOS install script

--- scripts/install-mac.sh
+++ scripts/install-mac.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 ROOT=$(git rev-parse --show-toplevel)
+cd $ROOT/dist/macos
 
 VERSION=$($ROOT/scripts/get-version.sh version)
-
 ZIP_FILE=gridmonger-v$VERSION-macos.zip
+unzip -q $ZIP_FILE
 
-cd dist/macos && \
-	unzip -q $ZIP_FILE && \
-	rm -rf /Applications/Gridmonger.app && \
-	mv Gridmonger.app /Applications
+rm -rf /Applications/Gridmonger.app
+mv Gridmonger.app /Applications
